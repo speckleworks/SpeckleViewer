25803ac8206097607c0c02ecb1b89dd05fe87a14
didimitrie <didimitrie@gmail.com>
2017-05-26 20:27:46 +0200

    adding ./dist for the sake of it

---
+++ dist/config.js
@@ -0,0 +1,9 @@
+var SpkAppConfig = {
+  serverUrl: 'http://localhost:8080',
+  allowGuestAccess: true,
+  // serverUrl: 'https://server.speckle.works',
+  // logoUrl: 'dist/logo.png'
+  logoUrl: 'https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2017-04-04/164468419442_9561f3ab8bcd823ebe33_132.png'
+}
+
+window.SpkAppConfig = SpkAppConfig
