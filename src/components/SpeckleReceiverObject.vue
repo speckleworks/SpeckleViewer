efdd86c3fdaf6579abc3ab6dd433343068cbcab6
mswaidan <mswaidan@gmail.com>
2018-02-11 02:42:58 +0000

    Forgot to add SpeckleReceiverObjects to previous commit

---
+++ src/components/SpeckleReceiverObject.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='spk-object'>
+    {{spkobj._id}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SpeckleReceiverObject',
+  props: { 
+    spkobj: { type: Object },
+  },
+  computed: {
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
