a1ff200a98cb95fe9726b7ae65041abb7e8e584d
mswaidan <mswaidan@gmail.com>
2018-02-26 01:14:12 +0000

    Added zoomToObject emitter to StreamList

--- src/components/SpeckleReceiverObject.vue
+++ src/components/SpeckleReceiverObject.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class='spk-object'>
-    {{spkobj._id}}
+  <div class='spk-object md-layout md-alignment-center-left md-dense'>
+    <md-layout-item class='spk-object-id'>{{spkobj._id}}</md-layout-item>
+    <md-layout-item>
+      <md-button class="md-icon-button" @click='select(spkobj._id)'>
+        <md-icon>zoom_in</md-icon>
+      </md-button>
+    </md-layout-item>
   </div>
 </template>
 
@@ -18,9 +23,15 @@ export default {
     }
   },
   methods: {
+    select(selection) {
+      bus.$emit('select-bus', selection)
+    }
   }
 }
 </script>
 
 <style scoped>
+.spk-object{
+  padding: 0 0 0 0;
+}
 </style>
