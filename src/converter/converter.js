08435a9a59a4fe78714f6e1550118128e9deca19
mswaidan <mswaidan@gmail.com>
2018-03-16 03:11:17 +0000

    Added support for planes

--- src/converter/converter.js
+++ src/converter/converter.js
@@ -12,12 +12,24 @@ export default {
       cb( null, point )
   },
   Vector( args, cb ) {
-    console.log(args.obj)
+    let v = new THREE.Vector3( ...args.obj.value )
+    //can't render this, vectors do not have origin point
     console.warn( 'TODO', args.obj.type)
   },
   Plane( args, cb ) {
-    console.log(args.obj)
-    console.warn( 'TODO', args.obj.type)
+    //make planeSize a setting in the viewer
+    let planeSize = 1
+    let position = new THREE.Vector3()
+    let v1 = new THREE.Vector3(0,0,1)
+    let v2 = new THREE.Vector3( ...args.obj.normal.value)
+    let q = new THREE.Quaternion()
+    q.setFromUnitVectors(v1, v2)
+    let geometry = new THREE.PlaneGeometry(planeSize, planeSize)
+    let plane = new THREE.Mesh( geometry, args.layer.threeMeshMaterial)
+    plane.position.fromArray(args.obj.origin.value)
+    plane.rotation.setFromQuaternion(q)
+    plane.hash = args.obj.hash
+    cb( null, plane )
   },
   Line( args, cb ) {
     let geometry = new THREE.Geometry()
@@ -34,8 +46,7 @@ export default {
     let origin = args.obj.center.value
     let radius = args.obj.radius
     let v1 = new THREE.Vector3(0,0,1)
-    let v2 = new THREE.Vector3()
-    v2.fromArray(args.obj.normal.value)
+    let v2 = new THREE.Vector3( ...args.obj.normal.value)
     let q = new THREE.Quaternion()
     q.setFromUnitVectors(v1, v2)
     let curve = new THREE.EllipseCurve(0, 0, radius, radius, 0, 2*Math.PI, false, 0)
@@ -48,14 +59,12 @@ export default {
     cb( null, circle)
   },
   Arc (args, cb) {
-    console.log(args.obj)
     let origin = args.obj.plane.origin.value
     let radius = args.obj.radius
     let startAngle = args.obj.startAngle
     let endAngle = args.obj.endAngle
     let v1 = new THREE.Vector3(0,0,1)
-    let v2 = new THREE.Vector3()
-    v2.fromArray(args.obj.plane.normal.value)
+    let v2 = new THREE.Vector3( ...args.obj.plane.normal.value)
     let q = new THREE.Quaternion()
     q.setFromUnitVectors(v1, v2)
     let curve = new THREE.EllipseCurve(0, 0, radius, radius, startAngle, endAngle, false, 0)
@@ -74,8 +83,7 @@ export default {
     let startAngle = args.obj.startAngle
     let endAngle = args.obj.endAngle
     let v1 = new THREE.Vector3(0,0,1)
-    let v2 = new THREE.Vector3()
-    v2.fromArray(args.obj.plane.normal.value)
+    let v2 = new THREE.Vector3( ...args.obj.plane.normal.value)
     let q = new THREE.Quaternion()
     q.setFromUnitVectors(v1, v2)
     let curve = new THREE.EllipseCurve(0, 0, xRadius, yRadius, startAngle, endAngle, false, 0)
@@ -88,14 +96,12 @@ export default {
     cb( null, arc)
   },
   Box( args, cb ) {
-    console.log(args.obj)
     let width = args.obj.xSize.end - args.obj.xSize.start
     let height = args.obj.ySize.end - args.obj.ySize.start
     let depth = args.obj.zSize.end - args.obj.zSize.start
     let origin = args.obj.basePlane.origin.value
     let v1 = new THREE.Vector3(0,0,1)
-    let v2 = new THREE.Vector3()
-    v2.fromArray(args.obj.basePlane.normal.value)
+    let v2 = new THREE.Vector3( ...args.obj.basePlane.normal.value)
     let q = new THREE.Quaternion()
     q.setFromUnitVectors(v1, v2)
     let geometry = new THREE.BoxGeometry(width, height, depth)
@@ -117,6 +123,7 @@ export default {
             cb( null, polyline )
   },
   Polycurve ( args, cb ){
+    console.log(args.obj)
     console.warn('TODO', args.obj.type)
   },
   Annotation ( args, cb ){
